75abd2a1a791822d9524d9684865cf72075f5a76
System Administrator <root@Princes-MacBook-Pro.local>
2024-10-08 17:10:24 +0200

    NUUUUUU

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 // import all routes
 import productRoutes from './routes/products.js'
