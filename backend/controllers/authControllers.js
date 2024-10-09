9641e3c9f888664c8125588455f1641a995f5f01
Prince Mbikayi <princembikayi@Princes-MacBook-Pro.local>
2024-10-10 00:03:12 +0200

    12:01

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -62,6 +62,7 @@ export const loginUser = catchAsyncErrors(async (req, res, next) => {
 
 // Logout User => /api/v1/logout
 export const logout = catchAsyncErrors(async (req, res, next) => {
+    
     // Vérifiez si l'utilisateur est déjà connecté
     if (!token) {
         return next(new ErrorHandler('You are already logged out', 400));
