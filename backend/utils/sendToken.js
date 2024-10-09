4100a393c88e24e34f11b46e531e0576946de952
Prince Mbikayi <princembikayi@Princes-MacBook-Pro.local>
2024-10-09 20:25:00 +0200

    NOW

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -4,16 +4,17 @@ export default (user, statusCode, res) => {
     const token = user.getJwtToken();
 
     // Options for cookie
-    const options = {
-        expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true,
-        sameSite: 'none',
-        secure: process.env.ENVIRONMENT == 'live'
-    };
-
-    res.status(statusCode).cookie("token", token, options).json({
-        token,
-    });
+    // Options for cookie
+const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    sameSite: "None",    // Allows cross-site cookies (frontend and backend on different domains)
+  };
+   
+  res.status(statusCode).cookie("token", token, options).json({
+    token,
+  });
+  
 };
