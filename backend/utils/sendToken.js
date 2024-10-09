42ede040c35577588f049e044531e47658d78ca4
abdul-rehman90 <abdul.rehman908909@gmail.com>
2024-10-09 15:57:13 +0500

    Fixed Cookies issue.

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -9,6 +9,8 @@ export default (user, statusCode, res) => {
             Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
+        sameSite: 'none',
+        secure: process.env.ENV === 'live'
     };
 
     res.status(statusCode).cookie("token", token, options).json({
