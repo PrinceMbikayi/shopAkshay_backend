1f880a87aecd1a9ac320b0a246f2020d58d46413
Prince Mbikayi <princembikayi@Princes-MacBook-Pro.local>
2024-10-10 07:33:08 +0200

    07:34

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -1,19 +1,24 @@
 export default (user, statusCode, res) => {
-  // Create JWT Token
-  const token = user.getJwtToken();
-
-  // Options for cookie
-  const options = {
-      expires: new Date(
-          Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",  // Cookie sécurisé en production
-      sameSite: "None",    // Permet les cookies inter-domaines (frontend/backend sur différents domaines)
+    // Create JWT Token
+    const token = user.getJwtToken();
+  
+    // Options for cookie
+    const options = {
+        expires: new Date(
+            Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "None",
+    };
+  
+    // Remove password from user object before sending response
+    user.password = undefined;
+  
+    res.status(statusCode).cookie("token", token, options).json({
+        success: true,
+        token,
+        user, // Include user data in the response
+    });
   };
-
-  res.status(statusCode).cookie("token", token, options).json({
-      success: true,
-      token,
-  });
-};
+  
